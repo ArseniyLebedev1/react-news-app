43a8fec108674e87e47905fc4fa377cea7f14d40
ArseniyLebedev1 <imarseniylebedev@gmail.com>
2025-12-10 09:56:49 +0300

    fix slider and banners scroll

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,29 +1,34 @@
+import { forwardRef } from "react";
 import styles from "./styles.module.css";
 
-const Categories = ({ categories, selectedCategory, setSelectedCategory }) => {
-  return (
-    <div className={styles.categories}>
-      <button
-        className={!selectedCategory ? styles.active : styles.item}
-        onClick={() => setSelectedCategory(null)}
-      >
-        All
-      </button>
-      {categories.map((category) => {
-        return (
-          <button
-            className={
-              selectedCategory === category ? styles.active : styles.item
-            }
-            onClick={() => setSelectedCategory(category)}
-            key={category}
-          >
-            {category}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+const Categories = forwardRef(
+  ({ categories, selectedCategory, setSelectedCategory }, ref) => {
+    return (
+      <div ref={ref} className={styles.categories}>
+        <button
+          className={!selectedCategory ? styles.active : styles.item}
+          onClick={() => setSelectedCategory(null)}
+        >
+          All
+        </button>
+        {categories.map((category) => {
+          return (
+            <button
+              className={
+                selectedCategory === category ? styles.active : styles.item
+              }
+              onClick={() => setSelectedCategory(category)}
+              key={category}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+);
+
+Categories.displayName = "Categories"
 
 export { Categories };
